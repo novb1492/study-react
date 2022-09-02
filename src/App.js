0b0e88bce7db18025a76a757c1b639c669381869
김준영 <novb1492@naver.com>
2022-09-02 20:21:32 +0900

    useEffect 사용법

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState,useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [keyword, setKeyword] = useState('s');
+
   console.log('a');
   const api=()=>{
     console.log('api');
@@ -10,11 +12,18 @@ function App() {
   const click=()=>{
     setNumber(number+1);
   }
+  const search=(event)=>{
+    console.log('search');
+    setKeyword(event.target.value);
+  }
   useEffect(api,[]);
-
+  useEffect(()=>{
+    console.log('search'+keyword);
+  },[keyword]);
   return (
     <div>
       {number}
+      <input type="text" onChange={search} value={keyword} ></input>
       <button onClick={click}>ff</button>
     </div>
   );
