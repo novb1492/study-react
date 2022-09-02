21ab89adcc9a8570fa13fb363f153849c3b06244
김준영 <novb1492@naver.com>
2022-09-02 17:34:10 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { useState,useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
+  console.log('a');
   const api=()=>{
     console.log('api');
   }
   const click=()=>{
     setNumber(number+1);
-    console.log('a');
-
   }
   useEffect(api,[]);
 
