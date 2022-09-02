d5dae027c6b119ee28d84a37753869900b9a2969
김준영 <novb1492@naver.com>
2022-09-02 17:32:48 +0900

    React.StrictMode 개발모드시 두번 씩 나옴

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const api=()=>{
+    console.log('api');
+  }
   const click=()=>{
     setNumber(number+1);
+    console.log('a');
+
   }
+  useEffect(api,[]);
+
   return (
     <div>
       {number}
