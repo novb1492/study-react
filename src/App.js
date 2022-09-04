8b30e6f4a18ab990526256c229613e2373d9056d
김준영 <novb1492@naver.com>
2022-09-04 12:26:02 +0900

    테스트겸 카카오지도 호출 다시 공부하러

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
   const [keyword, setKeyword] = useState('s');
-
   console.log('a');
-  const api=()=>{
-    console.log('api');
+  const click = () => {
+    setNumber(number + 1);
   }
-  const click=()=>{
-    setNumber(number+1);
-  }
-  const search=(event)=>{
+  const search = (event) => {
     setKeyword(event.target.value);
   }
-  useEffect(api,[]);
+
   useEffect(()=>{
-    if(keyword.length>5){
-      console.log('search'+keyword);
+    var container = document.getElementById('map');
+    var options = {
+      center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3
+    };
+    new window.kakao.maps.Map(container, options);
+    }, []);
+  useEffect(() => {
+    if (keyword.length > 5) {
+      console.log('search' + keyword);
     }
-  },[keyword]);
+  }, [keyword]);
   return (
     <div>
+      <div id="map" style={{width:"500px", height:"400px"}}></div> 
       {number}
       <input type="text" onChange={search} value={keyword} ></input>
       <button onClick={click}>ff</button>
